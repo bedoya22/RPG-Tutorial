c50201d313c723260b07178097de326f7dca2730
EiTaNBaRiBoA <eitanshapirshtein@gmail.com>
2021-09-16 10:50:50 +0300

    Implementing serverlist

--- Assets/Scenes/MainMenu/ServerList/ServerInformation.cs
+++ Assets/Scenes/MainMenu/ServerList/ServerInformation.cs
@@ -1,18 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using MLAPI;
 
 public class ServerInformation : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Text serverName, ServerStatus;
     void Start()
     {
-        
-    }
+        //         ServerConnection advertConnection = new ServerConnection();
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        //         // Connect to the ListServer
+        //         advertConnection.Connect("127.0.0.1", 9423);
+
+        //         // Create some data about our server
+        //         Dictionary<string, object> data = new Dictionary<string, object>
+        // {
+        //     { "Players", 25 },
+        //     { "Name", "This is the name" }
+        // };
+
+        //         // Announce our server to the ListServer
+        //         advertConnection.StartAdvertisment(data);
+        //     }
+
     }
 }

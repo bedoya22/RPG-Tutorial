1583e20e34d8067d4decc5c9bbe4ecd6a3067029
EiTaNBaRiBoA <eitanshapirshtein@gmail.com>
2021-09-16 11:10:52 +0300

    Serverinformation for each tab

--- Assets/Scenes/MainMenu/ServerList/ServerInformation.cs
+++ Assets/Scenes/MainMenu/ServerList/ServerInformation.cs
@@ -3,27 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using MLAPI;
-
-public class ServerInformation : MonoBehaviour
+using MLAPI.Transports;
+public class ServerInformation : NetworkBehaviour
 {
     public Text serverName, ServerStatus;
-    void Start()
+
+
+    private void Start()
     {
-        //         ServerConnection advertConnection = new ServerConnection();
-
-        //         // Connect to the ListServer
-        //         advertConnection.Connect("127.0.0.1", 9423);
-
-        //         // Create some data about our server
-        //         Dictionary<string, object> data = new Dictionary<string, object>
-        // {
-        //     { "Players", 25 },
-        //     { "Name", "This is the name" }
-        // };
-
-        //         // Announce our server to the ListServer
-        //         advertConnection.StartAdvertisment(data);
-        //     }
-
     }
+
 }

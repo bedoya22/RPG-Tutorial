76f456ccb7e32cd24b231bc47737520728d7019e
EiTaNBaRiBoA <eitanshapirshtein@gmail.com>
2021-09-16 07:38:32 +0300

    Working on overrides

--- Assets/Scenes/MainMenu/KeyMapping/Key.cs
+++ Assets/Scenes/MainMenu/KeyMapping/Key.cs
@@ -2,14 +2,52 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.InputSystem;
 
 public class Key : MonoBehaviour
 {
     public Text action, keyButton;
+    private string bindingPath; // the index of the binding effective path
 
-    public void onCreation(string action, string keyButton)
+    [SerializeField] private InputActionReference movement;
+    [SerializeField] private Button button;
+    private GameplayInput gamePlayInput;
+
+    private InputActionRebindingExtensions.RebindingOperation rebindingOperation;
+
+
+    public void StartRebinding()
+    {
+        button.interactable = false;
+        this.keyButton.text = "Waiting for input";
+        rebindingOperation = movement.action.PerformInteractiveRebinding().
+        OnMatchWaitForAnother(0.1f).OnComplete(operation => RebindComplete()).Start();
+
+    }
+
+    private void RebindComplete()
+    {
+        this.keyButton.text = InputControlPath.ToHumanReadableString(bindingPath, InputControlPath.HumanReadableStringOptions.OmitDevice);
+        button.interactable = true;
+        DebugKeyBindings();
+        movement.action.ApplyBindingOverride(movement.action.bindings[0]);
+        rebindingOperation.Dispose();
+    }
+    public void UpdateText(string action, string keyButton, string path)
     {
         this.action.text = action;
         this.keyButton.text = keyButton;
+        this.bindingPath = path;
+    }
+    private void DebugKeyBindings()
+    {
+        foreach (InputBinding binding in movement.action.bindings)
+        {
+            if (binding.isComposite) continue; // checks if it's not a binding but a composite type like 2DVector
+
+            //Getting the binding name to change the text later
+            string str = InputControlPath.ToHumanReadableString(binding.path, InputControlPath.HumanReadableStringOptions.OmitDevice);
+            Debug.Log(str);
+        }
     }
 }
---
+++ Assets/Scenes/MainMenu/KeyMapping/KeyMapping.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+public class KeyMapping : MonoBehaviour
+{
+    public Key keyMappingPreFab;
+    //public InputAction input; // to add a custome made input action
+    [SerializeField] UnityEngine.InputSystem.PlayerInput playerInput;
+    void Start()
+    {
+        // getting what type of hardware does he use Keyboard or gamepad
+        Debug.Log(playerInput.currentControlScheme);
+
+        //Creating the new GameplayInput class as in the asset inputsystem
+        GameplayInput gamePlayInput = new GameplayInput();
+        // Iteration through each binding. this allows also you not only get the specific binding but also passing it's path to change later
+        foreach (InputBinding binding in gamePlayInput.Player.Movement.bindings)
+        {
+            if (binding.isComposite) continue; // checks if it's not a binding but a composite type like 2DVector
+
+            //Getting the binding name to change the text later
+            string str = InputControlPath.ToHumanReadableString(binding.effectivePath, InputControlPath.HumanReadableStringOptions.OmitDevice);
+            Debug.Log(str);
+            //binding.name is the binding name that is written in the gameplayinput.cs generated file
+            Instantiate<Key>(keyMappingPreFab, Vector3.zero, Quaternion.identity, this.transform).UpdateText(binding.name, str, binding.path);
+        }
+
+    }
+
+}
+
+
+// Debug.Log(binding.path.Substring(binding.path.LastIndexOf('/') + 1)); // says the binding name
+// Debug.Log(playerInput.currentActionMap.ToString());
+// playerInput.SwitchCurrentActionMap("UI"); // change action map in the defaultinput system from Player to UI
+// Debug.Log(playerInput.currentActionMap.ToString());
